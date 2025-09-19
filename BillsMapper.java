a0661edf6f78e2e1ac5cb80c16a9e721ba8a1157
Zero1223first <151729921+Zero1223first@users.noreply.github.com>
2025-09-19 16:41:13 +0800

    账单，账单类型，用户的接口

---
+++ BillsMapper.java
@@ -0,0 +1,16 @@
+package com.sxt.sys.mapper;
+
+import com.sxt.sys.domain.Bills;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author pzq
+ * @since 2025-09-18
+ */
+public interface BillsMapper extends BaseMapper<Bills> {
+
+}
---
+++ BilltypeMapper.java
@@ -0,0 +1,16 @@
+package com.sxt.sys.mapper;
+
+import com.sxt.sys.domain.Billtype;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author pzq
+ * @since 2025-09-18
+ */
+public interface BilltypeMapper extends BaseMapper<Billtype> {
+
+}
